c6471a38deed93a6ec3902b93640f76489f17ec2
Andrew Fassler <fasslerandrew@gmail.com>
2025-06-18 19:17:56 -0700

    Adjust layout to be responsi: semi complete

--- personal-portfolio/src/App.tsx
+++ personal-portfolio/src/App.tsx
@@ -4,33 +4,37 @@ import './App.css'
 
 function App() {
   return (
-    <section className="about-me">  
-      <h2 className="about-heading"> About Me</h2>
+    <div className="app-container">
 
-      <div className="about-content">
-        <div className="andrew-box">
-          <img src={Andrew} alt ="Andrew" className="about-image" />
-        </div>  
+      <section className="about-me">  
+        <h2 className="about-heading"> About Me</h2>
 
-        <div className="right-side">
-          <p className="my-name">Andrew Fassler<br/>SoSy Undergrad</p>
+        <div className="about-content">
+          <div className="andrew-box">
+            <img src={Andrew} alt ="Andrew" className="about-image" />
+          </div>  
 
-        <div className="about-box">
-          <p className="about-text"> My name is Andrew Fassler and I am a Software 
-            Systems Undergraduate at Simon Fraser University with experience 
-            in  C++, Java, HTML, and CSS. While most of my programming experience 
-            is through school I am working toward building personal projects by 
-            learning React and Tailwind. I am also trying to familiar with Github and 
-            industry practices (through CMPT 276) to smoothly transition myself from 
-            school to real-world development.
-          </p>
+          <div className="right-side">
+            <p className="my-name">Andrew Fassler<br/>SoSy Undergrad</p>
+
+          <div className="about-box">
+            <p className="about-text"> My name is Andrew Fassler and I am a Software 
+              Systems Undergraduate at Simon Fraser University with experience 
+              in  C++, Java, HTML, and CSS. While most of my programming experience 
+              is through school I am working toward building personal projects by 
+              learning React and Tailwind. I am also trying to familiar with Github and 
+              industry practices (through CMPT 276) to smoothly transition myself from 
+              school to real-world development. 
+              
+              
+            </p>
+          </div>
+          </div>
         </div>
-        </div>
-      </div>
-    </section>
+      </section>
   
   
-
+    </div>
   );
 }
 
